25c448e06a56321d1f4efd0874201de31b7086d9
剧中人 <mail@bh-lay.com>
2021-06-18 22:45:26 +0800

    feat: 开发聚合搜索入口

--- src/components/search-entrance.vue
+++ src/components/search-entrance.vue
@@ -1,48 +1,100 @@
 <style lang="stylus" scoped>
 .search-entrance
-  width 400px
+  width 450px
   .tab-list
     display flex
   .tab-item
-    height 24px
-    padding 0 16px
-    line-height 24px
+    height 40px
+    padding 0 30px
+    line-height 40px
     font-size 12px
     color #555
     background rgba(255, 255, 255, 0.5)
+    cursor pointer
     &.active
       background #fff
 .search-input
-  height 50px
+  height 54px
+  border-radius 4px
+  border 1px solid #ddd
   background #fff
 </style>
 
 <template>
   <div class="search-entrance">
     <div class="tab-list">
-      <div class="tab-item">百度</div>
-      <div class="tab-item active">谷歌</div>
-      <div class="tab-item">stackoverflow</div>
-      <div class="tab-item">caniuse</div>
+      <div
+        :class="[
+          'tab-item',
+          engine.name === selectedEngineName ? 'active' : '',
+        ]"
+        v-for="engine in searchEngineConfig"
+        :key="engine.name"
+        @click="selectedEngineName = engine.name"
+      >
+        {{ engine.label }}
+      </div>
     </div>
-    <div class="search-input"></div>
+    <div class="search-input">
+      <input
+        type="text"
+        v-model="searchText"
+        placeholder="搜索"
+        @keydown="handleKeydown"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref } from 'vue';
 
 export default {
   setup() {
-    const count = ref(0);
-    const inc = () => {
-      count.value++;
-    };
-
+    const searchEngineConfig = [
+      {
+        name: 'baidu',
+        label: '百度',
+        url: 'https://www.baidu.com/s?ie=UTF-8&wd=[kw]',
+      },
+      {
+        name: 'google',
+        label: '谷歌',
+        url: 'https://www.baidu.com/s?ie=UTF-8&wd=[kw]',
+      },
+      {
+        name: 'caniuse',
+        label: 'caniuse',
+        url: 'https://www.caniuse.com/?search=[kw]',
+      },
+      {
+        name: 'stackoverflow',
+        label: 'stackoverflow',
+        url: 'https://www.baidu.com/s?ie=UTF-8&wd=[kw]',
+      },
+    ];
+    const selectedEngineName = ref(searchEngineConfig[0].name);
+    const searchText = ref('');
     return {
-      count,
-      inc,
+      searchEngineConfig,
+      selectedEngineName,
+      searchText,
     };
+  },
+  methods: {
+    handleKeydown(e) {
+      console.log('12122', e);
+      if (e.keyCode === 13) {
+        this.search();
+      }
+    },
+    search() {
+      let searhKeyword = encodeURIComponent(this.searchText);
+      let searchEngine = this.searchEngineConfig.filter(
+        (engine) => engine.name === this.selectedEngineName
+      )[0] || this.searchEngineConfig[0];
+      window.open(searchEngine.url.replace('[kw]', searhKeyword));
+    },
   },
 };
 </script>
