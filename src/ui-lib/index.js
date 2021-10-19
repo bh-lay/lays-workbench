753eebc5d2ca9f29bdf05323291938b136d567bb
剧中人 <mail@bh-lay.com>
2021-10-19 19:49:55 +0800

    feat: 增加 mdi 通用图标库

--- src/ui-lib/index.js
+++ src/ui-lib/index.js
@@ -1,7 +1,12 @@
 import clickOutSide from "./directives/click-out-side";
+import VueMdijs from 'vue-mdijs';
+import { mdiClose } from '@mdi/js';
 
 export default {
   install(app) {
     app.use(clickOutSide)
+    // mdi 图标
+    VueMdijs.add({ mdiClose });
+    app.use(VueMdijs)
   }
 }
