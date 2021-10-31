58c1c5fee805d071028993e46a2d4117a018e28e
剧中人 <mail@bh-lay.com>
2021-10-31 17:06:28 +0800

    feat: 初步开发本地数据库支持
    (cherry picked from commit 2f066fc347c27ff1c5f921431b3e9c1cc436960e)

---
+++ src/database/db.js
@@ -0,0 +1,35 @@
+
+function getIDBObject() {
+  return window.indexedDB ||
+    window.mozIndexedDB ||
+    window.webkitIndexedDB ||
+    window.msIndexedDB;
+}
+
+export function isSupportIDB() {
+  var indexedDB = getIDBObject()
+  return !!(indexedDB && indexedDB.open)
+}
+export function getIDBRequest() {
+  return new Promise((resolve, reject) => {
+    var indexedDB = getIDBObject()
+    var request = indexedDB.open('workbench', 1);
+    request.onerror = function(event) {
+      let error = new Error('建立数据库连接失败！')
+      error.__detail = event
+      reject(error)
+    }
+    request.onerror = function(event) {
+      let error = new Error('建立数据库连接失败！')
+      error.__detail = event
+      reject(error)
+    }
+    request.onsuccess = function() {
+      resolve(request.result)
+    }
+    request.onupgradeneeded = function(event) {
+      const db = request.result
+      resolve(db)
+    }
+  })
+}
