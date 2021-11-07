f01287037ac7bd1d9faf14c6ea719e69d3e0432c
剧中人 <mail@bh-lay.com>
2021-11-07 23:41:27 +0800

    feat: 开发图片 base64 功能

--- src/components/widgets/img-to-base.vue
+++ src/components/widgets/img-to-base.vue
@@ -4,45 +4,54 @@
   box-sizing border-box
   height 100%
   padding 16px
-  .side
-    margin-right 20px
-    .caption
-      height 10px
-      flex-grow 1
-    .title
-      margin-bottom 4px
-      line-height 20px
-      font-weight bold
-      font-size 18px
-      color #fff
-    .desc
-      margin-bottom 28px
-      line-height 16px
-      font-size 12px
-      color #adebaf
-    button
-      width 80px
-      height 30px
-      border none
-      border-radius 4px
-      cursor pointer
-      background #91cf93
-      color #27722a
-      transition .5s
-      &:active,
-      &:focus
-        outline none
-      &:hover
-        background #fff
-  .drag-area
-    display flex
+.side
+  margin-right 20px
+  .caption
+    height 10px
     flex-grow 1
-    align-items center
-    justify-content center
-    border 1px dashed #8cd98f
-    span
-      font-size 12px
-      color #85e088
+  .title
+    margin-bottom 4px
+    line-height 20px
+    font-weight bold
+    font-size 18px
+    color #fff
+  .desc
+    margin-bottom 28px
+    line-height 16px
+    font-size 12px
+    color #adebaf
+  button
+    width 80px
+    height 30px
+    border none
+    border-radius 4px
+    cursor pointer
+    background #91cf93
+    color #27722a
+    transition .5s
+    &:active,
+    &:focus
+      outline none
+    &:hover
+      background #fff
+.drag-area
+  display flex
+  flex-grow 1
+  align-items center
+  justify-content center
+  border 1px dashed #8cd98f
+  transition .2s
+  span
+    font-size 12px
+    color #85e088
+  &.active
+    background #70c273
+.hidden-input
+  position absolute
+  width 0
+  height 0
+  opacity 0
+  visibility hidden
 </style>
 
 <template>
@@ -50,14 +59,20 @@
     <div class="side">
       <div class="title">图片base64</div>
       <div class="desc">小图嵌入更容易</div>
-      <button>上传</button>
+      <button @click="$refs.input.click()">上传</button>
     </div>
-    <div class="drag-area">
+    <div :class="['drag-area', isDragOver ? 'active' : '']" ref="drag_area">
       <span>拖拽图片至此处</span>
     </div>
+    <input
+      ref="input"
+      type="file"
+      accept="image/*"
+      class="hidden-input"
+      @change="handleInputChange"
+    />
   </div>
   <modal v-model:visible="regVisualVisible" >
-    <iframe :src="iframeSrc" visible="0"></iframe>
   </modal>
 </template>
 
@@ -68,6 +83,7 @@ export default {
     const regVisualVisible = ref(false)
     const quickInput = ref('')
     const iframeSrc = ref('')
+    const isDragOver = ref(false)
     function showRegVisual() {
       let quickInputValue = quickInput.value
       let queryStr = ''
@@ -88,7 +104,38 @@ export default {
       quickInput,
       showRegVisual,
       iframeSrc,
+      isDragOver,
     };
+  },
+  mounted() {
+    const dragAreaNode = this.$refs.drag_area
+    if (!dragAreaNode) {
+      return
+    }
+    dragAreaNode.addEventListener('dragover', e => {
+      e.stopPropagation()
+      e.preventDefault()
+      e.dataTransfer.dropEffect = 'copy'
+      this.isDragOver = true
+    })
+    dragAreaNode.addEventListener('dragleave', e => {
+      this.isDragOver = false
+    })
+    dragAreaNode.addEventListener("drop", e => {
+      e.stopPropagation()
+      e.preventDefault()
+      var files = e.dataTransfer.files
+      this.handleSelectFile(files[0])
+      this.isDragOver = false
+    })
+  },
+  methods: {
+    handleInputChange(event) {
+      this.handleSelectFile(event.target.files[0])
+    },
+    handleSelectFile(file) {
+      console.log('file', file)
+    },
   },
 };
 </script>
