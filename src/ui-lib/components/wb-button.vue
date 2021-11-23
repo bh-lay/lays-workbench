ca32dbf4923c5f19648a838de59cd75515a8b7c7
剧中人 <mail@bh-lay.com>
2021-11-24 00:21:23 +0800

    feat:增加按钮组件

---
+++ src/ui-lib/components/wb-button.vue
@@ -0,0 +1,35 @@
+<style lang="stylus" scoped>
+.wb-button
+  padding 8px 20px
+  background #f4f4f4
+  border 1px solid #ddd
+  border-radius 4px
+  line-height 16px
+  font-size 14px
+  color #333
+  cursor pointer
+  transition .15s
+  &:hover
+    background #eee
+    border-color #ccc
+  &:active
+    background #ddd
+    border-color #ccc
+</style>
+
+<template>
+  <button :class="['wb-button', 'wb-button-' + type]">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: ''
+    }
+  },
+};
+</script>
