75916bca2fe947e1707a58a01eecb750aff7e6e0
剧中人 <mail@bh-lay.com>
2021-12-21 23:29:45 +0800

    feat: 增加全局 loading 效果

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,32 @@
-import { createApp } from 'vue'
+import { createApp, nextTick } from 'vue'
 import App from './App.vue'
 import uiLib from './ui-lib/index'
 import { initAppConfig } from './assets/ts/app-config'
 import { initVariables } from './assets/ts/css-variables'
 import './assets/stylus/index.styl'
+function initApp() {
+  initAppConfig()
+  initVariables()
 
-initAppConfig()
-initVariables()
+  const app = createApp(App)
+  app.use(uiLib)
+  app.mount('#app')
+}
+function removeMask() {
+  const maskNode: HTMLElement | null = document.querySelector('.app-mask')
+  if (!maskNode) {
+    return
+  }
 
-const app = createApp(App)
-app.use(uiLib)
-app.mount('#app')
+  const styleAtrribute = maskNode.style
+  styleAtrribute.setProperty('transition', '0.8s')
+  nextTick(() => {
+    styleAtrribute.setProperty('opacity', '0')
+  })
+  setTimeout(() => {
+    maskNode.parentNode?.removeChild(maskNode)
+  }, 1000)
+}
+
+initApp()
+removeMask()
