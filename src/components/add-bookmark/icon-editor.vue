995a35d2fd7acd531e76f0c4ac11b64d446dff3a
剧中人 <mail@bh-lay.com>
2022-01-09 15:58:16 +0800

    feat: 重写图标编辑器模块

--- src/components/add-bookmark/icon-editor.vue
+++ src/components/add-bookmark/icon-editor.vue
@@ -15,10 +15,12 @@
     width 50px
     flex-grow 1
     text-align center
-  &:hover,
-  &.active
-    background #2e333e
+  &:hover
+    background #181b20
     color #c1c5cd
+  &:active,
+  &.active
+    background #16181d
 input
   display block
   width 20px
@@ -38,11 +40,13 @@ input
   color #727e97
   transition .15s
   svg
-    width 14px
+    width 20px
+    height 20px
   &:hover
-    background #292e38
-  &:active
-    background #555e72
+    background #181b20
+  &:active,
+  &.active
+    background #16181d
 .crab-message
   width 10px
   flex-grow 1
@@ -58,10 +62,10 @@ input
       <span>{{ iconTypeLabel }}</span>
       <template #body>
         <v-dropdown-item
-          v-for="iconTypeItem in iconTypeList"
-          :key="iconTypeItem.value"
-          :class="[iconType === iconTypeItem.value ? 'active' : '']"
-          @click="iconType = iconTypeItem.value"
+          v-for="iconTypeItem in iconTypeConfigList"
+          :key="iconTypeItem.name"
+          :active="iconType === iconTypeItem.name"
+          @click="iconType = iconTypeItem.name"
         >
           {{ iconTypeItem.label }}
         </v-dropdown-item>
@@ -90,122 +94,153 @@ input
 </template>
 
 <script lang="ts">
-import { ref, watch } from 'vue'
+import { ref, watch, PropType } from 'vue'
 import { BookmarkIcon } from '@/database/entity/bookmark'
-const iconTypeList = [
+
+const iconTypeConfigList = [
   {
-    value: 'text',
+    name: 'text',
     label: '文字图标',
+    default: '好看',
   },
   {
-    value: 'mdi',
+    name: 'mdi',
     label: 'mdi图标',
+    default: 'mdi-heart-pulse',
   },
   {
-    value: 'crab',
+    name: 'crab',
     label: '抓取图标',
+    default: '',
   },
 ]
-// 根据 icon type 获取对应文字说明
-function getIconTypeLabel (iconType: string) {
-  for(let i = 0; i < iconTypeList.length; i++) {
-    if (iconTypeList[i].value === iconType) {
-      return iconTypeList[i].label
+// 根据 iconType 获取对应配置
+function getIconTypeConfig (iconType: string) {
+  for(let i = 0; i < iconTypeConfigList.length; i++) {
+    if (iconTypeConfigList[i].name === iconType) {
+      return iconTypeConfigList[i]
     }
   }
-  return '文字'
+  return iconTypeConfigList[0]
 }
-function formatIconData(iconConfig: BookmarkIcon) {
+
+/**
+ * 从图标数据，获取图标类型，用户输入值
+ *   mdi:play   ->  ['mdi', 'mdi-play']
+ *   text:play  ->  ['text', 'play']
+ *   crab       ->  ['crab', '']
+ */
+function decodeModelValue(iconConfig: BookmarkIcon) {
   const iconSplit = (iconConfig || '').split(':')
-  let types = ''
-  let value = ''
-  let userEditText = ''
   if (iconSplit[0] === 'mdi') {
-    types = 'mdi'
-    value = iconSplit[1]
-    userEditText = 'mdi-' + iconSplit[1]
+    return ['mdi', 'mdi-' + iconSplit[1]]
   } else if (iconConfig === 'crab') {
-    types = 'crab'
-    value = ''
-    userEditText = ''
-  } else {
-    types = 'text'
-    value = '淦'
-    userEditText = iconSplit[1]
+    return ['crab', '']
   }
-  return {
-    types,
-    value,
-    userEditText,
-  }
+  return ['text', iconSplit[1]]
 }
+
+/**
+ * 从图标类型、用户输入值，生成图标数据
+ *   ['mdi', 'mdi-play']  ->  mdi:play
+ *   ['text', 'play']     ->  text:play
+ *   ['crab', '']         ->  crab
+ */
+function encodeModelValue(newIconType: string, inputValue: string): BookmarkIcon {
+  if (newIconType === 'mdi') {
+    const iconName = (inputValue || '').replace(/^mdi-/, '')
+    // 经历过 replace 前后，值若不相等，则认为是合法的 mdi 配置
+    if (inputValue !== iconName) {
+      return `mdi:${iconName}`
+    } else {
+      // 否则，更正数据
+      return 'mdi:'
+    }
+  } else if (newIconType === 'crab') {
+    return 'crab'
+  }
+  return `text:${inputValue || ''}`
+}
+
 export default {
   props: {
     modelValue: {
-      type: String,
-      default: null,
+      type: String as PropType<BookmarkIcon>,
+      default: 'text: ',
     },
   },
   emits: ['update:modelValue'],
-  setup(props, context) {
+  setup(props: {
+    modelValue: BookmarkIcon
+  }, context) {
     const iconType = ref('')
-    const iconTypeLabel = ref('')
-    const iconValue = ref('')
     const inputValue = ref('')
-    // 从参数解析数据
-    function parseFromModelValue(newValue: string) {
-      const { types, value, userEditText } = formatIconData(newValue as BookmarkIcon)
-      if (types === iconType.value && value === iconValue.value) {
+    const iconTypeLabel = ref('')
+
+    // 从应用图标数据，更新编辑器视图
+    function applyBookmarkIcon(bookmarkIcon: BookmarkIcon) {
+      // 从图标数据，获取图标类型，用户输入值
+      const [ newIconType, newInputValue ] = decodeModelValue(bookmarkIcon)
+      // 当图标类型和用户输入值相同时，不处理
+      if (newIconType === iconType.value && newInputValue === inputValue.value) {
         return
       }
-      iconType.value = types
-      iconValue.value = value
-      inputValue.value = userEditText
-      iconTypeLabel.value = getIconTypeLabel(types)
+      // 更新数据
+      iconType.value = newIconType
+      inputValue.value = newInputValue
+      iconTypeLabel.value = getIconTypeConfig(newIconType).label
     }
-    watch(iconType, newIconType => {
-      // 获取对应的说明文字
-      iconTypeLabel.value = getIconTypeLabel(newIconType)
-      // 生成对应的默认值，并获取文本
-      let newModelValue: BookmarkIcon = 'crab'
-      if (newIconType === 'mdi') {
-        newModelValue = 'mdi:play'
-      } else if (newIconType === 'text') {
-        newModelValue = 'text:好看'
-      } else {
-        newModelValue = 'crab'
+    // 监听用户交互操作行为
+    function watchUserIntractive() {
+      // 监听图标类型变化
+      const unWatchIconType = watch(iconType, newIconType => {
+        let defaultInputValue = getIconTypeConfig(newIconType).default
+        let newModelValue: BookmarkIcon = encodeModelValue(newIconType, defaultInputValue)
+        applyBookmarkIcon(newModelValue)
+        context.emit('update:modelValue', newModelValue)
+      })
+      // 监听用户输入
+      const unWatchInput = watch(inputValue, newInputValue => {
+        // 避免粘贴时出现重复的 mdi-，尝试去删除
+        if (newInputValue.match(/^mdi-mdi-/)) {
+          newInputValue = newInputValue.replace(/^mdi-/, '')
+        }
+        const newModelValue = encodeModelValue(iconType.value, newInputValue)
+        applyBookmarkIcon(newModelValue)
+        context.emit('update:modelValue', newModelValue)
+      })
+      // 标记解除监听回调
+      unWatchUserIntractive = () => {
+        unWatchIconType()
+        unWatchInput()
       }
-      parseFromModelValue(newModelValue)
-      context.emit('update:modelValue', newModelValue)
-    })
-    watch(inputValue, newValue => {
-      const newIconType = iconType.value
-      // 生成对应的默认值，并获取文本
-      let newModelValue = ''
-      if (newIconType === 'mdi') {
-        iconValue.value = newValue.replace(/^mdi-/, '')
-        newModelValue = 'mdi:' + iconValue.value
-      } else if (newIconType === 'text') {
-        iconValue.value = newValue
-        newModelValue = 'text:' + newValue
-      } else {
-        // 理论上执行不到这一行
-        return
+    }
+    // 解除用户交互操作行为监听
+    let unWatchUserIntractive = () => {
+      // nothind
+    }
+    // 监听父组件传入的数据变动
+    watch(
+      () => props.modelValue,
+      (newValue: BookmarkIcon) => {
+        // 解除用户操作监听
+        unWatchUserIntractive()
+        // 更新编辑器数据
+        applyBookmarkIcon(newValue)
+        // 创建用户操作监听
+        watchUserIntractive()
+      },
+      {
+        immediate: true,
       }
-      context.emit('update:modelValue', newModelValue)
-    })
-    watch(() => props.modelValue, (newValue: string) => {
-      parseFromModelValue(newValue)
-    })
-    parseFromModelValue(props.modelValue)
+    )
     return {
       iconType,
       iconTypeLabel,
-      iconValue,
-      iconTypeList,
+      iconTypeConfigList,
       inputValue,
       openMdiHelp() {
-        window.open('https://material.iconhelper.cn/', 'about:blank')
+        window.open('https://material.iconhelper.cn/', 'material-help')
       },
     }
   },
