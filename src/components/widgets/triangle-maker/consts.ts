bc1e66a4910cd7599bec7dc8016e4038a6ac677b
剧中人 <mail@bh-lay.com>
2022-01-28 22:05:13 +0800

    feat: 完成三角形编辑器基础编辑流程开发

--- src/components/widgets/triangle-maker/consts.ts
+++ src/components/widgets/triangle-maker/consts.ts
@@ -1,25 +1,107 @@
-const uniformStyle = `width: 0;
-height: 0;
-border-style: solid;`
-const triTemplate = [
-  `border-width: 20px 30px 20px 0;
-border-color: transparent #007bff transparent transparent;`,
-  `border-width: 0 30px 30px 0;
-border-color: transparent #007bff transparent transparent;`,
-  `border-width: 30px 20px 0 20px;
-border-color: #007bff transparent transparent transparent;`,
-  `border-width: 30px 30px 0 0;
-border-color: #007bff transparent transparent transparent;`,
-  `border-width: 20px 0 20px 30px;
-border-color: transparent transparent transparent #007bff;`,
-  `border-width: 30px 0 0 30px;
-border-color: transparent transparent transparent #007bff;`,
-  `border-width: 0 20px 30px 20px;
-border-color: transparent transparent #007bff transparent;`,
-  `border-width: 0 0 30px 30px;
-border-color: transparent transparent #007bff transparent;`,
+export type triTemplateMaker = {
+  borderWidth: (a: number, b: number, c: number) => string
+  borderColor: (color: string) => string
+  default: () => [a: number, b: number, c: number]
+}
+const triTemplate: triTemplateMaker[] = [
+  // 1
+  {
+    borderWidth(a: number, b: number, c: number) {
+      return `${a}px ${c || 30}px ${b}px 0`
+    },
+    borderColor(color: string) {
+      return `transparent ${color} transparent transparent`
+    },
+    default() {
+      return [20, 20, 30]
+    },
+  },
+  // 2
+  {
+    borderWidth(a: number, b: number) {
+      return `0 ${a}px ${b}px 0`
+    },
+    borderColor(color: string) {
+      return `transparent ${color} transparent transparent`
+    },
+    default() {
+      return [30, 30, -1]
+    },
+  },
+  // 3
+  {
+    borderWidth(a: number, b: number, c: number) {
+      return `${c}px ${a}px 0 ${b}px`
+    },
+    borderColor(color: string) {
+      return `${color} transparent transparent transparent`
+    },
+    default() {
+      return [20, 20, 30]
+    },
+  },
+  // 4
+  {
+    borderWidth(a: number, b: number) {
+      return `${a}px ${b}px 0 0`
+    },
+    borderColor(color: string) {
+      return `${color} transparent transparent transparent`
+    },
+    default() {
+      return [30, 30, -1]
+    },
+  },
+  // 5
+  {
+    borderWidth(a: number, b: number, c: number) {
+      return `${a}px 0 ${b}px ${c}px`
+    },
+    borderColor(color: string) {
+      return `transparent transparent transparent ${color}`
+    },
+    default() {
+      return [20, 20, 30]
+    },
+  },
+  // 6
+  {
+    borderWidth(a: number, b: number) {
+      return `${a}px 0 0 ${b}px`
+    },
+    borderColor(color: string) {
+      return `transparent transparent transparent ${color}`
+    },
+    default() {
+      return [30, 30, -1]
+    },
+  },
+  // 7
+  {
+    borderWidth(a: number, b: number, c: number) {
+      return `0 ${a}px ${c}px ${b}px`
+    },
+    borderColor(color: string) {
+      return `transparent transparent ${color} transparent`
+    },
+    default() {
+      return [20, 20, 30]
+    },
+  },
+  // 8
+  {
+    borderWidth(a: number, b: number) {
+      return `0 0 ${a}px ${b}px`
+    },
+    borderColor(color: string) {
+      return `transparent transparent ${color} transparent`
+    },
+    default() {
+      return [30, 30, -1]
+    },
+  },
 ]
 
 export function getTriCssTemplate(index: number) {
-  return uniformStyle + '\n' + triTemplate[index]
+  return triTemplate[index]
 }
--- src/components/widgets/triangle-maker/main.vue
+++ src/components/widgets/triangle-maker/main.vue
@@ -12,15 +12,49 @@
   font-size 28px
   color #afc4d4
 .label
-  margin 20px 0 5px
+  margin 20px 0 10px
   font-size 14px
   color #afc4d4
+.isosceles
+  float right
+  height 28px
+  padding 0 5px
+  line-height 28px
+  font-size 12px
+  border-radius 4px
+  cursor pointer
+  transition .2s
+  svg,
+  span
+    display inline-block
+    vertical-align text-bottom
+    height 16px
+  svg
+    width 16px
+    margin-right 4px
+    fill transparent
+  span
+    line-height 16px
+  &:hover
+    background #476685
+  &:active
+    background #364f63
+  &.active
+    background #364f63
+    svg
+      fill currentColor
+.slider-item
+  margin 10px 0
+  .value
+    line-height 12px
+    font-size 12px
+    color #29333d
 .main
   display flex
   flex-direction column
   width 100px
   flex-grow 1
-.preview
+.preview-canvas
   display flex
   align-items center
   justify-content center
@@ -29,6 +63,11 @@
   background-image linear-gradient(45deg, rgba(0,0,0,.25) 25%, transparent 0, transparent 75%, rgba(0,0,0,.25) 0),linear-gradient(45deg, rgba(0,0,0,.25) 25%, transparent 0, transparent 75%, rgba(0,0,0,.25) 0)
   background-position 0 0, 10px 10px
   background-size 20px 20px
+.triangle
+  width 0
+  height 0
+  border-style solid
+  transition .2s
 .code
   padding 50px 80px 70px
   line-height 1.5
@@ -59,44 +98,85 @@
       />
       <div class="label">
         尺寸调整
+        <div
+          :class="{
+            isosceles: true,
+            active: customIsosceles,
+          }"
+          @click="customIsosceles = !customIsosceles"
+        >
+          <v-mdi name="mdi-check" />
+          <span>等边三角形</span>
+        </div>
       </div>
-      {{ value1 }}
-      <v-slider
-        v-model="value1"
-        :min="2"
-        :max="30"
-        :step="1"
-      />
-      <v-slider
-        v-model="value2"
-        :min="2"
-        :max="30"
-        :step="1"
-      />
-      <v-slider
-        v-model="value3"
-        :min="2"
-        :max="30"
-        :step="1"
-      />
+      <div class="slider-item">
+        <div class="value">
+          {{ customValue1 }}px
+        </div>
+        <v-slider
+          v-model="customValue1"
+          :min="2"
+          :max="60"
+          :step="1"
+        />
+      </div>
+      <div
+        v-if="!customIsosceles"
+        class="slider-item"
+      >
+        <div class="value">
+          {{ customValue2 }}px
+        </div>
+        <v-slider
+          v-model="customValue2"
+          :min="2"
+          :max="60"
+          :step="1"
+        />
+      </div>
+      <div
+        v-if="customValue3 > -1"
+        class="slider-item"
+      >
+        <div class="value">
+          {{ customValue3 }}px
+        </div>
+        <v-slider
+          v-model="customValue3"
+          :min="2"
+          :max="60"
+          :step="1"
+        />
+      </div>
+      <div class="label">
+        颜色
+      </div>
+      <input
+        v-model="customColor"
+        type="text"
+      >
     </div>
     <div class="main">
-      <div
-        ref="previewRef"
-        class="preview"
-      />
-      <div
-        ref="codeRef"
-        class="code"
-      />
+      <div class="preview-canvas">
+        <div
+          class="triangle"
+          :style="{
+            borderWidth: screenBorderWidth,
+            borderColor: screenBorderColor,
+          }"
+        />
+      </div>
+      <div class="code">
+        <span>width: 0;<br>height: 0;<br>border-style: solid;<br>border-width: {{ screenBorderWidth }};<br>border-color: {{ screenBorderColor }};</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Ref, ref, watch, onMounted } from 'vue'
+import { Ref, ref, watch } from 'vue'
 import TriangleSelector from './triangle-selector.vue'
-import { getTriCssTemplate } from './consts'
+import { getTriCssTemplate, triTemplateMaker } from './consts'
 export default {
   name: 'TriangleMakerWidgets',
   components: { TriangleSelector },
@@ -108,32 +188,52 @@ export default {
   },
   setup(props) {
     const selectedIndex = ref(props.defaultIndex)
-    const previewRef: Ref<HTMLElement | null> = ref(null)
-    const codeRef: Ref<HTMLElement | null> = ref(null)
-    const value1 = ref(70)
-    const value2 = ref(50)
-    const value3 = ref(70)
-    function updateTriangleType(value: number) {
-      const template = getTriCssTemplate(value - 1)
-      const previewNode = previewRef.value
-      const codeNode = codeRef.value
-      if (!previewNode || !codeNode) {
+    const customValue1 = ref(30)
+    const customValue2 = ref(20)
+    const customValue3 = ref(30)
+    const customColor = ref('#007bff')
+    // 是否为等腰三角形
+    const customIsosceles = ref(true)
+    const screenBorderWidth: Ref<string> = ref('')
+    const screenBorderColor: Ref<string> = ref('')
+    let usedTriTemplate: triTemplateMaker | null = null
+    function updateTriangleValue() {
+      if (!usedTriTemplate) {
         return
       }
-      previewNode.innerHTML = `<div style="${template}"></div>`
-      codeNode.innerHTML = template
+      if (customIsosceles.value) {
+        customValue2.value = customValue1.value
+      }
+      screenBorderWidth.value = usedTriTemplate.borderWidth(customValue1.value, customValue2.value, customValue3.value)
+      screenBorderColor.value = usedTriTemplate.borderColor(customColor.value)
     }
-    watch(selectedIndex, updateTriangleType)
-    onMounted(function () {
-      updateTriangleType(selectedIndex.value)
-    })
-
+    watch(
+      selectedIndex,
+      function updateTriangleType(typeIndex: number) {
+        usedTriTemplate = getTriCssTemplate(typeIndex - 1)
+        if (!usedTriTemplate) {
+          return
+        }
+        const [a, b, c] = usedTriTemplate.default()
+        customValue1.value = a || 0
+        customValue2.value = b || 0
+        customValue3.value = c || 0
+        screenBorderWidth.value = usedTriTemplate.borderWidth(customValue1.value, customValue2.value, customValue3.value)
+        screenBorderColor.value = usedTriTemplate.borderColor(customColor.value)
+      },
+      {
+        immediate: true,
+      }
+    )
+    watch([customValue1, customValue2, customValue3, customColor, customIsosceles], updateTriangleValue)
     return {
-      value1,
-      value2,
-      value3,
-      previewRef,
-      codeRef,
+      customValue1,
+      customValue2,
+      customValue3,
+      customColor,
+      customIsosceles,
+      screenBorderWidth,
+      screenBorderColor,
       selectedIndex,
       handleSelected(index: number) {
         console.log('index', index)
