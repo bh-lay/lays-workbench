75bb4bb5577ba287df3c12a24d07dd0ac56c87ce
剧中人 <mail@bh-lay.com>
2022-02-01 21:51:58 +0800

    feat: 获取默认数据增加缓存，避免冗余的重复加载

--- src/database/utils/init-bookmark-to-db.ts
+++ src/database/utils/init-bookmark-to-db.ts
@@ -1,13 +1,23 @@
 import { bookmarkCountManager, bookmarkInsertListManager } from '../manager/bookmark-manager'
 import { bookmarkOriginData } from '../entity/bookmark'
 
-export function loadDefaultData() {
+type defaultData = {
+  bookmarks: bookmarkOriginData[]
+}
+// 默认数据缓存
+let defaultDataCache: defaultData | null = null
+// 获取默认数据方法
+export function loadDefaultData(): Promise<defaultData> {
+  if (defaultDataCache) {
+    return Promise.resolve(defaultDataCache)
+  }
   return fetch('/static-api/default-data.json')
     .then(res => res.json())
     .then(json => {
-      return {
+      defaultDataCache = {
         bookmarks: (json.bookmarks || []) as bookmarkOriginData[],
       }
+      return defaultDataCache
     })
 }
 // 自动填充默认数据
