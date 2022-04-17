2e9499504944c550c5d1d6e1e44ce0805edfc073
剧中人 <mail@bh-lay.com>
2022-04-17 16:38:09 +0800

    feat: 统一button 交互

--- src/ui-lib/components/v-button.vue
+++ src/ui-lib/components/v-button.vue
@@ -6,27 +6,18 @@
   border-radius 4px
   line-height 16px
   font-size 14px
-  color #333
-  cursor pointer
-  transition .15s
-.v-button-default
   background #2f2f37
   border 1px solid #1c1c21
   color #bdbdc7
+  cursor pointer
+  transition .15s
   &:hover
     background #3a3a40
   &:active
     background #212126
-.v-button-white
-  background #f4f4f4
-  border 1px solid #ddd
-  color #333
-  &:hover
-    background #eee
-    border-color #ccc
-  &:active
-    background #ddd
-    border-color #ccc
+  &:focus
+    outline none
+    border-color #2154ba
 </style>
 
 <template>
