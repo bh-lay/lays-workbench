57a6d6a19e72e5a574c44df16ee5e11f84a6222b
bh-lay <mail@bh-lay.com>
2025-02-21 23:09:57 +0800

    feat: add code diff widgets

---
+++ src/components/widgets/code-diff/index.vue
@@ -0,0 +1,82 @@
+<style lang="stylus" scoped>
+.formatter-widgets
+  height 100%
+  display flex
+  flex-direction column
+  align-items center
+  justify-content center
+  cursor pointer
+  background #900965
+  transition .2s
+  &:hover
+    background #730a51
+svg
+  width 32px
+  height 32px
+  margin 5px 0 10px
+  fill #fff
+  &.small
+    margin 0
+    width 24px
+.title
+  margin-bottom 2px
+  line-height 20px
+  font-size 15px
+  font-weight bold
+  color #fff
+.desc
+  line-height 20px
+  font-size 12px
+  color rgba(255, 255, 255, .6)
+</style>
+<template>
+  <div
+    class="formatter-widgets"
+    @click="open"
+  >
+    <v-mdi
+      name="mdi-vector-difference"
+      :class="{
+        small: widgetsSize === BookmarkSize.small
+      }"
+    />
+    <template v-if="widgetsSize !== BookmarkSize.small">
+      <div class="title">
+        文本对比
+      </div>
+      <div class="desc">
+        高效文本差异分析
+      </div>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed } from 'vue'
+import { openBookmark } from '@/assets/ts/bookmark-utils'
+import { Bookmark, BookmarkSize } from '@database/entity/bookmark'
+
+export default {
+  name: 'CodeDiffWidgetsButton',
+  props: {
+    data: {
+      type: Bookmark,
+      default() {
+        return new Bookmark({})
+      },
+    },
+  },
+  setup(props: {
+    data: Bookmark
+  }) {
+    const widgetsSize = computed(() => props.data.size)
+    return {
+      widgetsSize,
+      BookmarkSize,
+      open() {
+        openBookmark(props.data)
+      },
+    }
+  },
+}
+</script>
